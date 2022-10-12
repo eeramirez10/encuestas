a68c7af451dc9d31b9979d1286ed34fd753cd0d0
Erick <eeramirez@tuvansa.com.mx>
2022-10-12 17:46:45 -0500

    cambio de puerto nodemailer

--- database/nodemailer.js
+++ database/nodemailer.js
@@ -2,8 +2,8 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
     host:"securemail25.carrierzone.com",
-    port:"465",
-    secure:true,
+    port:"1025",
+    secure:false,
     auth:{
         user:"eeramirez@tuvansa.com.mx",
         pass:"Ag7348pp**"
